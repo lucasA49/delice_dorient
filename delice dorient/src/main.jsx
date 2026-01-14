4c7cbdedd65ee764cc85233c3d7886dfb7037a00
lucasA49 <lucasaksu@gmail.com>
2026-01-14 16:50:44 +0100

    mise en place de tailwinds css

--- delice dorient/src/main.jsx
+++ delice dorient/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  // -------------------------
+  // PAGES PUBLIQUES
+  // -------------------------
+  {
+    path: "/",
+    element: <App />,
+  },
+
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
